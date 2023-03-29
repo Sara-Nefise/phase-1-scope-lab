3b9dc6f3c9c06085b9ec9f0ed81fc9721290a188
saranafisa <sara_nafisa@protonmail.com>
2023-03-30 01:08:15 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName='bob';
+function upperCaseCustomerName(){
+     customerName = customerName.toUpperCase();
+return customerName
+}
+
+
+function setBestCustomer(){
+  return  bestCustomer = 'not bob';
+  }
+console.log( setBestCustomer())
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob'
+    return bestCustomer
+}
+const leastFavoriteCustomer='some data'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer='edit'
+    return leastFavoriteCustomer
+}
